47f0fbb64d3b49a44e819bcd2b806494fd4e094f
Swapnil Rao <swapnil.rao1@gmail.com>
2023-11-14 22:09:04 +0530

    added death screen mechanism and a way to play the game again by pressing down on the space key

--- platformer.py
+++ platformer.py
@@ -8,6 +8,7 @@ screen=pygame.display.set_mode((800,400))
 pygame.display.set_caption("Disoriented")
 clock=pygame.time.Clock()
 score=pygame.font.Font('font/Pixeltype.otf',30)
+gamestatus=True
 
 #surfaces
 skys=pygame.image.load('graphics/Sky.png').convert()
@@ -17,13 +18,13 @@ scores=score.render('SCORE', False, 'pink')
 scorer=scores.get_rect(topleft=(5,-3))
 
 hounds=pygame.image.load('graphics/snail/tile000.png').convert_alpha()
-#38 24
-hounds = pygame.transform.scale(hounds, (76, 44))
+'''38 24'''
+hounds = pygame.transform.scale(hounds, (80, 48))
 houndr=hounds.get_rect(midbottom=(900,300))
 
-players=pygame.image.load('graphics/Player/player.png').convert_alpha()
-#37 39
-players = pygame.transform.scale(players, (64,68))
+players=pygame.image.load('graphics/Player/player1.png').convert_alpha()
+'''37 39 -> 44 60'''
+players = pygame.transform.scale(players, (75,100))
 playerr=players.get_rect(midbottom=(50,300))
 playerg=0
 
@@ -32,34 +33,43 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if playerr.bottom==300:
-                    playerg=-20    
-                else:
-                    pass
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if playerr.collidepoint(event.pos):
-                playerg=-20
-        if event.type == pygame.KEYUP:
-            print("keydown")
+        if gamestatus==True:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if playerr.bottom==300:
+                        playerg=-20    
+                    else:
+                        pass
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if playerr.collidepoint(event.pos):
+                    playerg=-20
+            if event.type == pygame.KEYUP:
+                print("keydown")
+        else:
+            if event.type==pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    gamestatus=True    
+                    houndr.right=800    
+
+    if gamestatus==True:
+        screen.blit(skys,(0,0))
+        screen.blit(grounds,(0,300))
+        screen.blit(scores,scorer)
+        houndr.right+=-5
+        if houndr.right<-100:
+            houndr.right=900
+        screen.blit(hounds,houndr)
+        playerg+=0.96
+        playerr.y+=playerg
+        if playerr.bottom>=300:
+            playerr.bottom=300
+        screen.blit(players,playerr)
+
+        if houndr.colliderect(playerr):
+            gamestatus=False
+
+    else:
+        screen.fill('red')
 
     pygame.display.update()
     clock.tick(60)
-    screen.blit(skys,(0,0))
-    screen.blit(grounds,(0,300))
-    screen.blit(scores,scorer)
-    houndr.right+=-5
-    if houndr.right<-100:
-        houndr.right=900
-    screen.blit(hounds,houndr)
-    playerg+=0.98
-    playerr.y+=playerg
-    if playerr.bottom>=300:
-        playerr.bottom=300
-    screen.blit(players,playerr)
-
-    if houndr.colliderect(playerr):
-        pygame.quit()
-    
-pygame.close()
